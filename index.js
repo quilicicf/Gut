548f0264bb1acdbde4ba0054042d5ff05a7cb8a1
quilicicf <cquilici@restlet.com>
2017-08-14 17:27:46 +0200

    :new: Implement branch #3

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const colors = require('colors');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const yargs = require('yargs');
 
 //////////////////////
 // RETRIEVE OPTIONS //
@@ -27,22 +28,13 @@ try {
 // PROCESS ARGUMENTS //
 ///////////////////////
 
-const arguments = process.argv.slice(2);
-
-const actions = {
-  clone: arguments => {
-    require('./lib/clone.js').clone(gutOptions, arguments);
-  },
-  groot: () => {
-    console.log(`Je s'appelle groot!`);
-  }
-};
-
-const firstArgument = _.pullAt(arguments, 0)[ 0 ];
-
-if (!firstArgument) {
-  console.error(`I need to display some help there, feel free to contribute if you need it.`.red);
-  return;
-}
-
-actions[ firstArgument ](arguments);
+yargs
+  .usage('usage: $0 <command>')
+  .command([ 'branch', 'b' ], `Displays the given remote's branches`, yargs => require('./lib/branch.js').branch(yargs))
+  // .command([ 'clone', 'cl' ], `Clones a repository`, yargs => require('./lib/clone.js').clone(yargs, gutOptions)
+  .command('groot', 'Display a random sentence, in French', () => {
+    console.log(`Je s'appelle Groot`);
+  })
+  .demandCommand(1, 'Specify the command you want to run!'.red)
+  .help()
+  .argv;
