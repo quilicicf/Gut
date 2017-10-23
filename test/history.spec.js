e40912da2718aedbf92965182a70e5eef5fd27b1
Cyprien Quilici <quilicicf@gmail.com>
2017-10-23 19:09:45 +0200

    Implement query-review feature #55 (#57)
    * :new: Implement feature query-review (#55)
    
    * :eyes: Code review #55

--- test/history.spec.js
+++ test/history.spec.js
@@ -1,9 +1,6 @@
-
 describe('History', () => {
-
-    test('It should return all commit history', () => {
-        // TODO replace it by a real test
-        expect(true).toBeTruthy();
-    });
-
+  test('It should return all commit history', () => {
+    // TODO replace it by a real test
+    expect(true).toBeTruthy();
+  });
 });
