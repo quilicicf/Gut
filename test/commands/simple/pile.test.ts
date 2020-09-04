79c231e56391390c50f306cc0f56cc6221ad55a4
quilicicf <quilicicf@gmail.com>
2020-09-04 17:54:45 +0200

    :new: Add history command

--- test/commands/simple/pile.test.ts
+++ test/commands/simple/pile.test.ts
@@ -1,12 +1,14 @@
 import { assertEquals } from '../../utils/assert.ts';
-import { resolve } from '../../../src/utils/path.ts';
-import { exec, execSequence, OutputMode } from '../../../src/utils/exec.ts';
+import { resolve } from '../../../src/dependencies/path.ts';
+import { exec, execSequence, OutputMode } from '../../../src/dependencies/exec.ts';
 
 import pileCommand from '../../../src/commands/simple/pile.ts';
 
 Deno.test('pile', async () => {
   const testRepositoryPath = await Deno.makeTempDir({ prefix: 'gut_test_pile' });
-  await Deno.chdir(testRepositoryPath);
+  Deno.chdir(testRepositoryPath);
+  const tmpDir = resolve(testRepositoryPath, '..');
+
   await exec('git init', { output: OutputMode.None });
   await Deno.mkdir(resolve(testRepositoryPath, 'nested'));
   await Deno.writeTextFile(resolve(testRepositoryPath, 'nested', 'witness'), 'witness');
@@ -19,16 +21,17 @@ Deno.test('pile', async () => {
   await Deno.writeTextFile(resolve(testRepositoryPath, 'updated'), 'toUpdate');
   await Deno.writeTextFile(resolve(testRepositoryPath, 'added'), 'added');
   await Deno.remove(resolve(testRepositoryPath, 'toRemove'));
-  await Deno.chdir(resolve(testRepositoryPath, 'nested'));
+  Deno.chdir(resolve(testRepositoryPath, 'nested'));
 
   const { handler: pile } = pileCommand;
   const output = await pile({ isTestRun: true });
 
+  Deno.chdir(tmpDir); // Don't remove cwd, duh
   await Deno.remove(testRepositoryPath, { recursive: true });
 
   assertEquals(output, `\
-[33m## [m[32mmaster[m
-[32mA[m  added
-[32mD[m  toRemove
-[32mA[m  updated`);
+## master
+A  added
+D  toRemove
+A  updated`);
 });
--- test/utils/assert.ts
+++ test/utils/assert.ts
@@ -1,9 +1,11 @@
 import {
   assertEquals,
+  assertMatch,
   assertArrayContains,
 } from 'https://deno.land/std@0.67.0/testing/asserts.ts';
 
 export {
   assertEquals,
+  assertMatch,
   assertArrayContains,
 };
