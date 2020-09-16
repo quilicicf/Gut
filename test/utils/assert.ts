24f079cbb8e90a5865c22c3281801d0b63a1a9c6
quilicicf <quilicicf@gmail.com>
2020-09-16 13:48:22 +0200

    :new: Add branch utilities

--- test/utils/assert.ts
+++ test/utils/assert.ts
@@ -1,11 +1,13 @@
 import {
   assertEquals,
   assertMatch,
+  assertThrows,
   assertArrayContains,
 } from 'https://deno.land/std@0.67.0/testing/asserts.ts';
 
 export {
   assertEquals,
   assertMatch,
+  assertThrows,
   assertArrayContains,
 };
