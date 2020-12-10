1e368b5061158443fb1eaeab7c841eaa0a6d6384
quilicicf <quilicicf@gmail.comgrba>
2020-12-10 11:02:20 +0100

    :triangular_ruler: Better responsibility barriers in commands

--- src/commands/simple/pile.ts
+++ src/commands/simple/pile.ts
@@ -1,3 +1,4 @@
+import log from '../../dependencies/log.ts';
 import { exec, OutputMode } from '../../dependencies/exec.ts';
 
 import { moveUpTop } from '../../lib/git.ts';
@@ -7,15 +8,19 @@ export const aliases = [ 'p' ];
 export const describe = 'Adds all changes in the repository';
 
 export function builder (yargs: any) {
-  return yargs.usage(`usage: gut pile [options]`);
+  return yargs.usage('usage: gut pile [options]');
 }
 
 export async function handler ({ isTestRun }: { isTestRun: boolean }) {
   await moveUpTop();
   await exec('git add . --all', { output: OutputMode.None });
 
-  const { output } = isTestRun
-    ? await exec('git -c color.status=never status --short --branch', { output: OutputMode.Capture })
-    : await exec('git -c color.status=always status --short --branch', { output: OutputMode.StdOut });
+  if (isTestRun) {
+    const { output } = await exec('git -c color.status=never status --short --branch', { output: OutputMode.Capture });
+    return output;
+  }
+
+  const { output } = await exec('git -c color.status=always status --short --branch', { output: OutputMode.Capture });
+  await log(Deno.stdout, output);
   return output;
 }
