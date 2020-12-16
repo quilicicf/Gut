b699fd11802ea70278da5de881aa497318577f83
quilicicf <quilicicf@gmail.com>
2020-12-16 09:55:28 +0100

    :new: Add replicate command

--- index.ts
+++ index.ts
@@ -3,34 +3,54 @@ import { yargs } from './src/dependencies/yargs.ts';
 import install from './src/commands/internals/install.ts';
 import { applyStyle, theme } from './src/dependencies/colors.ts';
 
+import { getConfiguration } from './src/configuration.ts';
+
 import * as pile from './src/commands/simple/pile.ts';
 import * as audit from './src/commands/simple/audit.ts';
 import * as thrust from './src/commands/simple/thrust.ts';
 import * as burgeon from './src/commands/simple/burgeon.ts';
 import * as history from './src/commands/simple/history.ts';
+import * as replicate from './src/commands/simple/replicate.ts';
 import * as divisions from './src/commands/simple/divisions.ts';
 
 // Install with: deno install --allow-run --allow-read="$FORGE" --name gd index.ts
-yargs()
-  .usage('usage: gut <command>')
+const main = async () => {
+  const yargsInstance: any = yargs();
+  const configuration = await getConfiguration();
 
-  // Public methods
-  .command(audit)
-  .command(burgeon)
-  .command(divisions)
-  .command(history)
-  .command(pile)
-  .command(thrust)
+  yargsInstance
+    .usage('usage: gut <command>')
+    .option('configuration', {
+      default: configuration,
+      type: 'string',
+      global: true,
+      hidden: true,
+    })
 
-  // Internals
-  .command(install)
+    // Public methods
+    .command(audit)
+    .command(burgeon)
+    .command(divisions)
+    .command(history)
+    .command(pile)
+    .command(replicate)
+    .command(thrust)
 
-  // To check that Gut is installed or just mess around
-  .command('groot', 'Display a random sentence, in French', () => log(Deno.stdout, 'Je s\'appelle Groot\n'))
+    // Internals
+    .command(install)
 
-  .demandCommand(1, applyStyle('Specify the command you want to run!', [ theme.error ]))
-  .strictCommands()
-  .help()
-  .version()
-  .epilogue('For more information, read the manual at https://github.com/quilicicf/Gut/blob/master/specs/user_documentation.md')
-  .parse(Deno.args);
+    // To check that Gut is installed or just mess around
+    .command('groot', 'Display a random sentence, in French', () => log(Deno.stdout, 'Je s\'appelle Groot\n'))
+
+    .demandCommand(1, applyStyle('Specify the command you want to run!', [ theme.error ]))
+    .strictCommands()
+    .help()
+    .version()
+    .epilogue('For more information, read the manual at https://github.com/quilicicf/Gut/blob/master/specs/user_documentation.md')
+    .parse(Deno.args);
+};
+
+main()
+  .catch(async (error) => {
+    await log(Deno.stderr, error.message); // TODO: better error handling?
+  });
---
+++ src/configuration.ts
@@ -0,0 +1,59 @@
+import { getTopLevel } from './lib/git.ts';
+import { resolve } from './dependencies/path.ts';
+import { exists } from './dependencies/fs.ts';
+
+export interface Account {
+  username: string,
+  password: string,
+  // password: { // TODO: encrypt password
+  //   crypt: string,
+  //   iv: string,
+  // },
+}
+
+export interface Tool {
+  account: Account,
+  server?: {}, // TODO: implement
+  ci?: {}, // TODO: implement
+  messaging?: {}, // TODO: implement
+}
+
+export interface GlobalGutConfiguration {
+  tools: { [ key: string ]: Tool },
+  preferredGitServer: string, // A key to a tool with server facet
+  repositoriesPath: string,
+}
+
+export interface RepositoryGutConfiguration {
+  reviewTool: string, // A key to a tool with review facet in global configuration
+}
+
+export interface FullGutConfiguration {
+  global: GlobalGutConfiguration,
+  repository?: RepositoryGutConfiguration,
+}
+
+export const CONFIGURATION_FILE_NAME = '.gut-config.json';
+export const FORGE_PATH = '/home/cyp/talend/forge';
+
+export async function getConfiguration (): Promise<FullGutConfiguration> {
+  const globalConfigurationPath = resolve(FORGE_PATH, CONFIGURATION_FILE_NAME);
+  const globalConfigurationAsJson = await Deno.readTextFile(globalConfigurationPath); // TODO: handle no configuration file
+  const globalConfiguration = JSON.parse(globalConfigurationAsJson) as GlobalGutConfiguration;
+
+  const currentRepositoryTopLevel = await getTopLevel();
+  if (!currentRepositoryTopLevel) { return { global: globalConfiguration }; }
+  const repositoryConfigurationPath = resolve(currentRepositoryTopLevel, CONFIGURATION_FILE_NAME);
+
+  if (await exists(repositoryConfigurationPath)) {
+    const repositoryConfigurationAsJson = await Deno.readTextFile(repositoryConfigurationPath);
+    const repositoryConfiguration = JSON.parse(repositoryConfigurationAsJson) as RepositoryGutConfiguration;
+
+    return {
+      global: globalConfiguration,
+      repository: repositoryConfiguration,
+    };
+  }
+
+  return { global: globalConfiguration };
+}
