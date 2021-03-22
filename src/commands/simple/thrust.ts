b9f425ceac7c073035ba3e77476eedc339a301e9
quilicicf <cquilici@restlet.com>
2021-03-22 13:45:19 +0100

    :up: Use force-with-lease for thrust command

--- src/commands/simple/thrust.ts
+++ src/commands/simple/thrust.ts
@@ -16,7 +16,7 @@ export async function builder (yargs: any) {
   return yargs.usage('usage: gut thrust [options]')
     .option('force', {
       alias: 'f',
-      describe: 'Force the push. This erases concurrent server-modifications', // TODO: implement safety net
+      describe: 'Force the push. This erases concurrent server-modifications',
       type: 'boolean',
     });
 }
@@ -26,7 +26,7 @@ export async function handler ({ force, isTestRun }: Args) {
   const currentBranchName = await getCurrentBranchName();
   const remote = remotes[ 0 ]; // TODO: prompt user when there are multiple remotes
 
-  const forceArg = force ? '--force' : '';
+  const forceArg = force ? '--force-with-lease' : '';
   const outputMode = isTestRun ? OutputMode.Capture : OutputMode.StdOut;
   return exec(`git push ${forceArg} --set-upstream ${remote} ${currentBranchName}`, { output: outputMode });
 }
