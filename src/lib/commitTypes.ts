66d8b0568e1f6e91fcc2914239f5385c58759f88
quilicicf <quilicicf@gmail.com>
2022-03-02 19:13:34 +0100

    :new: Add doc commit type for angular message format

--- src/lib/commitTypes.ts
+++ src/lib/commitTypes.ts
@@ -4,6 +4,7 @@ import { stoyle, theme } from '../dependencies/stoyle.ts';
 export const COMMIT_TYPES: Record<string, SelectOption> = {
   FEAT: { value: 'feat', name: stoyle`${'feat'} when working on a feature`({ nodes: [ theme.strong ] }) },
   FIX: { value: 'fix', name: stoyle`${'fix'} when working on a bug fix`({ nodes: [ theme.strong ] }) },
+  DOC: { value: 'doc', name: stoyle`${'doc'} when working on documentation`({ nodes: [ theme.strong ] }) },
   TEST: { value: 'test', name: stoyle`${'test'} when working on tests`({ nodes: [ theme.strong ] }) },
   CHORE: { value: 'chore', name: stoyle`${'chore'} when working on anything else`({ nodes: [ theme.strong ] }) },
 };
