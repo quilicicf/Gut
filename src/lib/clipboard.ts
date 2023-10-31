3960731af328edda04caaf27f10db639a91f867b
quilicicf <quilicicf@gmail.com>
2023-10-31 15:53:13 +0100

    :triangular_ruler: Migrate Deno.run to Deno.Command, part I

--- src/lib/clipboard.ts
+++ src/lib/clipboard.ts
@@ -1,16 +1,18 @@
 import { getPermissionOrExit } from './getPermissionOrExit.ts';
 
-const writeText = async (cmd: string[], data: string): Promise<number> => {
-  const process = Deno.run({
-    cmd,
+const writeText = async (cmd: string, args: string[], data: string): Promise<number> => {
+  const command = new Deno.Command(cmd, {
+    args,
     stdin: 'piped',
-    stdout: 'piped',
-    stderr: 'piped',
+    stdout: null,
+    stderr: null,
   });
-  await process?.stdin?.write(new TextEncoder().encode(data));
-  await process?.stdin?.write(new TextEncoder().encode(undefined));
-  process?.stdin?.close();
-  const { code } = await process.status();
+  const process = await command.spawn();
+  const writer = process.stdin.getWriter();
+  await writer.write(new TextEncoder().encode(data));
+  await writer.releaseLock();
+  await process.stdin.close();
+  const { code } = await process.output();
   return code;
 };
 
@@ -29,19 +31,19 @@ const COMMANDS_BY_OS: CommandsByOs = {
   windows: {
     programName: 'powershell',
     async writeText (data: string): Promise<number> {
-      return writeText([ 'powershell', '-noprofile', '-command', '$input|Set-Clipboard' ], data);
+      return writeText('powershell', [ '-noprofile', '-command', '$input|Set-Clipboard' ], data);
     },
   },
   darwin: {
     programName: 'pbcopy',
     async writeText (data: string): Promise<number> {
-      return writeText([ 'pbcopy' ], data);
+      return writeText('pbcopy', [], data);
     },
   },
   linux: {
     programName: 'xclip',
     async writeText (data: string): Promise<number> {
-      return writeText([ 'xclip', '-selection', 'clipboard' ], data);
+      return writeText('xclip', [ '-selection', 'clipboard' ], data);
     },
   },
 };
