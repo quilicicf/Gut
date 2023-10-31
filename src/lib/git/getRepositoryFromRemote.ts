df80a9790d92031e654c7b712e76324cf3e25c71
quilicicf <quilicicf@gmail.com>
2023-10-31 17:45:24 +0100

    :triangular_ruler: Migrate Deno.run to Deno.Command, part VI

--- src/lib/git/getRepositoryFromRemote.ts
+++ src/lib/git/getRepositoryFromRemote.ts
@@ -1,15 +1,17 @@
 import { DEFAULT_REMOTE } from './remotes.ts';
 
 export async function getRepositoryFromRemote (remote: string = DEFAULT_REMOTE.name) {
-  const process = Deno.run({
-    cmd: [ 'git', 'remote', 'show', '-n', remote ],
-    stdin: 'null',
-    stdout: 'piped',
-    stderr: 'null',
-  });
+  const process = new Deno.Command(
+    'git',
+    {
+      args: [ 'remote', 'show', '-n', remote ],
+      stdin: 'null',
+      stdout: 'piped',
+      stderr: 'null',
+    },
+  ).spawn();
 
-  const remoteDescription = await process.output();
-  process.close();
+  const { stdout: remoteDescription } = await process.output();
 
   const remoteDescriptionAsString = new TextDecoder().decode(remoteDescription);
   const regex = /^[ ]+Fetch URL: git@[^:]+:([^/]+)\/(.*?)\.git$/m;
